f37e5498bbac0fd2815ce795ea6b23f81ddedec6
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-05-11 00:11:49 +0530

    Databse and the Dao

--- app/src/main/java/com/hasindu/noteme/database/NoteDatabase.kt
+++ app/src/main/java/com/hasindu/noteme/database/NoteDatabase.kt
@@ -7,29 +7,28 @@ import androidx.room.RoomDatabase
 import com.hasindu.noteme.model.Note
 
 @Database(entities = [Note::class], version = 1)
-abstract class  NoteDatabase:RoomDatabase(){
-    abstract fun getNoteDao():NoteDao
-    companion object{
-        @Volatile // changes ehema ikamanata pennana  one nisa meka danne
+abstract class NoteDatabase : RoomDatabase() {
+    abstract fun getNoteDao(): NoteDao
 
-        // data base eka  heavy nisa singlleton pattern ekak use karnwa one welwata ganna
-        private  var instance : NoteDatabase? = null
+    companion object {
+        @Volatile // changes ikamanta anith  thread walata visible wenna one nisa
+        private var instance: NoteDatabase? = null
         private val LOCK = Any()
 
-        operator fun invoke(context:Context) = instance?:
-        synchronized(LOCK){
-            instance?:
-            createDatabase(context).also{
+
+        //singleton function , databse eka bara wadi nisa
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also {
                 instance = it
             }
         }
 
-            private fun createDatabase (context: Context){
-                Room.databaseBuilder(
-                    context.applicationContext,
-                    NoteDatabase::class.java,
-                    name = "note_db"
-                ).build() //return
-            }
+        private fun createDatabase(context: Context): NoteDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                NoteDatabase::class.java,
+                name = "note_db"
+            ).build()
+        }
     }
 }
